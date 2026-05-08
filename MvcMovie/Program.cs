013c8952a514f895b9a9f989be389a81856fd240
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2026-05-08 23:28:43 +0200

    requirements added

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddDbContext<MvcMovieContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddValidation();
 
 var app = builder.Build();
 
